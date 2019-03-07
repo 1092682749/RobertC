4e807dded7effeff7f1795c57ce880d44c21c380
1092682749 <dyz1092682749@gmail.com>
2019-03-07 18:01:49 +0800

    no message

---
+++ Event_Object_Synchronization/Event_Object_Synchronization/Event_Object_Synchronization.cpp
@@ -0,0 +1,35 @@
+﻿
+#include "pch.h"
+#include <Windows.h>
+#include <iostream>
+
+// 线程函数:函数要去文件映射对象里去读取数据
+void ThreadFun(LPVOID lpParam)
+{
+
+}
+int main()
+{
+	// 打开指定文件
+	HANDLE hFile = CreateFile(L"C:/Users/i-robert/Desktop/a.txt",
+		GENERIC_ALL,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+	// 为打开的文件创建file-mapping-object
+	HANDLE hFileMapping = CreateFileMapping(hFile,
+		NULL,
+		PAGE_EXECUTE_READWRITE,
+		0,
+		0,
+		L"File_a_txt");
+	LPVOID fileAddress = MapViewOfFile(
+		hFileMapping,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		0);
+	return 0;
+}
