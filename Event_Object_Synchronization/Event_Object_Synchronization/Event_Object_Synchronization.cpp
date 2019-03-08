874c731a13e63e3bacdc791abc24c5d7795c6e37
1092682749 <dyz1092682749@gmail.com>
2019-03-08 09:41:13 +0800

    事件同步线程

--- Event_Object_Synchronization/Event_Object_Synchronization/Event_Object_Synchronization.cpp
+++ Event_Object_Synchronization/Event_Object_Synchronization/Event_Object_Synchronization.cpp
@@ -3,9 +3,37 @@
 #include <Windows.h>
 #include <iostream>
 
+#define THREADCOUNT 4
+
+DWORD THREADIDS[THREADCOUNT];
+HANDLE THREADS[THREADCOUNT];
+
 // 线程函数:函数要去文件映射对象里去读取数据
 void ThreadFun(LPVOID lpParam)
 {
+	HANDLE tFm;
+	LPVOID address;
+	DWORD dwWaitResult;
+	std::cout << "Thread run...\n";
+	HANDLE *phEvent = (HANDLE*)lpParam;
+	dwWaitResult = WaitForSingleObject(*phEvent, INFINITE);
+	std::cout << "Compelet wait...\n";
+	switch (dwWaitResult)
+	{
+	case WAIT_OBJECT_0:
+		std::cout << "Wait result is: WAIT_OBJECT_0\n";
+		tFm = OpenFileMapping(FILE_MAP_ALL_ACCESS,
+			TRUE,
+			L"File_a_txt");
+		address = MapViewOfFile(tFm, FILE_MAP_READ, 0, 0, 0);
+		std::cout << (LPSTR)address << "\n";
+		CloseHandle(tFm);
+		break;
+	default:
+		std::cout << "Wait faild\n";
+		break;
+	}
+	
 
 }
 int main()
@@ -31,5 +59,34 @@ int main()
 		0,
 		0,
 		0);
+	// 创建事件对象并初始为无信号
+	HANDLE hEvent = CreateEvent(
+		NULL,
+		TRUE,
+		FALSE,
+		L"WriteEvent"
+	);
+	for (int i = 0; i < THREADCOUNT; i++)
+	{
+		THREADS[i] = CreateThread(
+			NULL,
+			NULL,
+			(LPTHREAD_START_ROUTINE)ThreadFun,
+			&hEvent,
+			0,
+			&THREADIDS[i]
+		);
+	}
+	Sleep(1000);
+	std::cout << "Change event state\n";
+	if (!SetEvent(hEvent))
+	{
+		std::cout << "SetEvent faild!";
+		return -2;
+	}
+	WaitForMultipleObjects(THREADCOUNT, THREADS, TRUE, INFINITE);
+	CloseHandle(hEvent);
+	CloseHandle(hFile);
+	CloseHandle(hFileMapping);
 	return 0;
 }
