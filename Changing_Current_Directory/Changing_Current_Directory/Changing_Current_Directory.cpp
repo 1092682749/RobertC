8724860a031b33ccdbaf50b74874f9e0f35e2d09
1092682749 <dyz1092682749@gmail.com>
2019-03-14 09:07:35 +0800

    no message

--- Changing_Current_Directory/Changing_Current_Directory/Changing_Current_Directory.cpp
+++ Changing_Current_Directory/Changing_Current_Directory/Changing_Current_Directory.cpp
@@ -9,20 +9,52 @@
 
 #define BUFSIZE MAX_PATH
 
-template<typename T>
-std::list<T> ergodicFileList(T t)
-{
 
-	LPWIN32_FIND_DATAA find_data;
-	if (!FindFirstFile("*", find_data))
+std::list<WIN32_FIND_DATA> ergodicFileList()
+{
+	std::list<WIN32_FIND_DATA> l;
+	HANDLE hFirstFile;
+	WIN32_FIND_DATA find_data;
+	LARGE_INTEGER liFileSize;
+	if ((hFirstFile = FindFirstFile(L"*", &find_data)) == INVALID_HANDLE_VALUE)
 	{
-		printf("find faild\n");
+		_tprintf(L"find faild\n");
 	}
-	
+	do
+	{
+		WIN32_FIND_DATA fd = find_data;
+		l.push_back(fd);
+		if (find_data.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY)
+		{
+			_tprintf(L"this is a directory\n");
+		}
+		else
+		{
+			liFileSize.LowPart = find_data.nFileSizeLow;
+			liFileSize.HighPart = find_data.nFileSizeHigh;
+			_tprintf(L"%s %ld bytes\n", find_data.cFileName, liFileSize.QuadPart);
+		} 
+	} while (FindNextFile(hFirstFile, &find_data));
+	return l;
+}
+typedef struct MyStruct
+{
+	int a;
+}*PMS;
+void test(PMS &s)
+{
+	s->a = 100;
+}
+void testN(MyStruct &s)
+{
+	s.a = 99;
+}
+void testP(PMS s)
+{
+	s->a = 1000;
 }
 int main()
 {
-	char c[] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssss.txt";
 	DWORD size = GetCurrentDirectory(0, NULL);
 	TCHAR *buf = new TCHAR[size];
 	size = GetCurrentDirectory(size, (LPWSTR)buf);
@@ -31,16 +63,29 @@ int main()
 	{
 		std::cout << (char*)(buf + i);
 	}
-	printf("\n");
-	BOOL b = SetCurrentDirectory(L"D:/");
+	_tprintf(L"\n");
+	BOOL b = SetCurrentDirectory(L"D:/a");
 	if (b)
 	{
-		printf("SetCurrentDirectory successful!\n");
+		_tprintf(L"SetCurrentDirectory successful!\n");
 	}
 	else
 	{
-		printf("Call SetCD faild! Error = %d\n", GetLastError());
+		_tprintf(L"Call SetCD faild! Error = %d\n", GetLastError());
 	}
 	
+	PMS p = (PMS)malloc(sizeof(MyStruct));
+	p->a = 10;
+	testN(*p);
+	test(p);
+	testP(p);
+	std::cout << p->a << "\n";
+	std::list<WIN32_FIND_DATA> lis = ergodicFileList();
+	std::list< WIN32_FIND_DATA>::iterator iter = lis.begin();
+		while (iter != lis.end())
+		{
+			WIN32_FIND_DATA fd = (*iter);
+			iter++;
+		}
 	return 0;
 }
