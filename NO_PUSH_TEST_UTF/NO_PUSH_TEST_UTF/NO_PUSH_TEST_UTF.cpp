18b2b5efa722ffcef481a63304592031a199f896
1092682749 <dyz1092682749@gmail.com>
2019-03-15 14:29:26 +0800

    no message

--- NO_PUSH_TEST_UTF/NO_PUSH_TEST_UTF/NO_PUSH_TEST_UTF.cpp
+++ NO_PUSH_TEST_UTF/NO_PUSH_TEST_UTF/NO_PUSH_TEST_UTF.cpp
@@ -1,4 +1,4 @@
-﻿// Append_File_To_Other_File.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// NO_PUSH_TEST_UTF.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "pch.h"
@@ -8,7 +8,6 @@
 
 int main(int argc, TCHAR *argv[])
 {
-	std::cout << "中文\n";
 	//if (argc < 3)
 	//{
 	//	_tprintf(L"Need Source File And Destination File\n");
@@ -63,3 +62,14 @@ int main(int argc, TCHAR *argv[])
 	CloseHandle(hDestinationFile);
 	return 0;
 }
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
